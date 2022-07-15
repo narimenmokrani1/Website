5e073eca15c84acb8c416fe678328b7d21aec580
narimenmokrani1 <narimenmokrani@gmail.com>
2022-07-15 12:03:49 -0500

    header transparent

--- narimen-website/src/App.js
+++ narimen-website/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <Header/>
-     <h1>Rimel's Food</h1>
+     {/* <h1>Rimel's Food</h1> */}
      
     </div>
   );
--- narimen-website/src/Components/Header/Header.js
+++ narimen-website/src/Components/Header/Header.js
@@ -2,14 +2,14 @@ import React from 'react';
 import style from './Header.css'
 function Header(props) {
     return (
-        <header>
-            <ul className='header'>
-                <li className='ligin'>Login</li>
-                <li className='food_list'>Food list</li>
-                <li className='home'>Home</li>
-            </ul>
-        </header>
-    );
+			<header>
+				<ul className='header transparent-bg'>
+					<li className='ligin'>Login</li>
+					<li className='food_list'>Food list</li>
+					<li className='home'>Home</li>
+				</ul>
+			</header>
+		);
 }
 
 export default Header;
