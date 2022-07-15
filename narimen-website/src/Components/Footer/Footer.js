693b3791244677ba6b6290ee12290dffcc880e87
narimenmokrani1 <narimenmokrani@gmail.com>
2022-07-15 14:51:04 -0500

    changed footer icons size

--- narimen-website/src/Components/Footer/Footer.js
+++ narimen-website/src/Components/Footer/Footer.js
@@ -12,14 +12,14 @@ function Footer(props) {
 					<a href='https://www.linkedin.com/in/narimen-mokrani-bb5a7a13b/'>
 						<LinkedInIcon
 							className='icon'
-							style={{ fontsize: 150, color: 'black' }}
+							style={{ fontSize: 80, color: 'black' }}
 						/>
 					</a>
 					<a href='https://github.com/narimenmokrani1'>
-						<GitHub style={{ fontsize: 80, color: 'black' }} />
+						<GitHub style={{ fontSize: 80, color: 'black' }} />
 					</a>
 					<a href='https://narimen-mokrani-portfolio.netlify.app/'>
-						<AccountCircle style={{ fontsize: 80, color: 'black' }} />
+						<AccountCircle style={{ fontSize: 80, color: 'black' }} />
 					</a>
 					<li className='copy-footer'>Rimel's Food &copy; 2022</li>
 				</ul>
