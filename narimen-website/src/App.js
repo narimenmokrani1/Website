6d5a19b118c56b6c2c918c3fc7784fef67c2a068
narimenmokrani1 <narimenmokrani@gmail.com>
2022-07-14 20:32:30 -0500

    first commit

---
+++ narimen-website/src/App.js
@@ -0,0 +1,14 @@
+import Header from './Components/Header/Header'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+     <h1>Rimel's Food</h1>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ narimen-website/src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './Header.css'
+function Header(props) {
+    return (
+        <header>
+            <ul className='header'>
+                <li className='ligin'>Login</li>
+                <li className='food_list'>Food list</li>
+                <li className='home'>Home</li>
+            </ul>
+        </header>
+    );
+}
+
+export default Header;
