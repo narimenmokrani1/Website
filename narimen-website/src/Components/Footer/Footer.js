757f77abc40ca7cb8e5f3ede89215726a9122982
narimenmokrani1 <narimenmokrani@gmail.com>
2022-07-15 14:13:03 -0500

    footer icons done

--- narimen-website/src/Components/Footer/Footer.js
+++ narimen-website/src/Components/Footer/Footer.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { GitHub } from '@mui/icons-material';
+import { AccountCircle } from '@mui/icons-material';
 import style from './Footer.css';
+import { color } from '@mui/system';
 function Footer(props) {
 	return (
 		<div>
 			<footer>
 				<ul className='footer-list'>
 					<a href='https://www.linkedin.com/in/narimen-mokrani-bb5a7a13b/'>
-						<LinkedInIcon className='icon' />
-						<li className='icon-text'>Narimen Mokrani</li>
+						<LinkedInIcon
+							className='icon'
+							style={{ fontsize: 150, color: 'black' }}
+						/>
+					</a>
+					<a href='https://github.com/narimenmokrani1'>
+						<GitHub style={{ fontsize: 80, color: 'black' }} />
+					</a>
+					<a href='https://narimen-mokrani-portfolio.netlify.app/'>
+						<AccountCircle style={{ fontsize: 80, color: 'black' }} />
 					</a>
 					<li className='copy-footer'>Rimel's Food &copy; 2022</li>
 				</ul>
