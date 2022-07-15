d801431beca4253b7a9666fad9bcc861752b7831
narimenmokrani1 <narimenmokrani@gmail.com>
2022-07-15 12:30:52 -0500

    footer

--- narimen-website/src/App.js
+++ narimen-website/src/App.js
@@ -1,4 +1,5 @@
 import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer';
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <Header/>
      {/* <h1>Rimel's Food</h1> */}
+     <Footer/>
      
     </div>
   );
--- narimen-website/src/Components/Footer/Footer.js
+++ narimen-website/src/Components/Footer/Footer.js
@@ -1,13 +1,18 @@
 import React from 'react';
-
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import style from './Footer.css';
 function Footer(props) {
 	return (
 		<div>
 			<footer>
-                <ul>
-                    
-                </ul>
-            </footer>
+				<ul className='footer-list'>
+					<a href='https://www.linkedin.com/in/narimen-mokrani-bb5a7a13b/'>
+						<LinkedInIcon className='icon' />
+						<li className='icon-text'>Narimen Mokrani</li>
+					</a>
+					<li className='copy-footer'>Rimel's Food &copy; 2022</li>
+				</ul>
+			</footer>
 		</div>
 	);
 }
