9cc55eb0f02f0d043d8072e940848d8639645c7d
narimenmokrani1 <narimenmokrani@gmail.com>
2022-07-15 12:21:57 -0500

    installing material ui

---
+++ narimen-website/src/Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Footer(props) {
+	return (
+		<div>
+			<footer>
+                <ul>
+                    
+                </ul>
+            </footer>
+		</div>
+	);
+}
+
+export default Footer;
