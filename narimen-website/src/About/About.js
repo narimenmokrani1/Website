03abb62da55cf9a62ddf37be40db4365a47e9a10
narimenmokrani1 <narimenmokrani@gmail.com>
2022-07-24 21:15:17 -0500

    working on the about

--- narimen-website/src/About/About.js
+++ narimen-website/src/About/About.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
 function About(props) {
-	const { url, path } = useRouteMatch();
 	return (
-		<div className='about__content'>
-			<ul className='about__list'>...</ul>
-			...
+		<div>
+			<h2>About</h2>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo facere
+				accusantium ut deleniti officia, sequi in eaque saepe cum hic quisquam
+				iure neque. Ipsum eveniet vero totam. Ea, repudiandae corporis?
+			</p>
 		</div>
 	);
 }
